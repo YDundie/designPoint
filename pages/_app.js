c06745e91b400c642f9d53d806925308d0e21ac7
YDundie <mustacmarko@yahoo.com>
2022-11-21 17:17:54 +0100

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '../styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above+
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Header></Header>
       <Component {...pageProps} />
       <Footer></Footer>
+      <Analytics />
     </div>
   );
 }
